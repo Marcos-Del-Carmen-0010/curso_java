a5a9ceb886da698b70d2bebfeb78d9a1e31287d0
Marcos-Del-Carmen-0010 <marcossachezdelcarmen@gmail.com>
2025-08-05 07:35:53 -0700

    algunas clases hijas

---
+++ src/s19_herenciaAndAbstract/SelectForm.java
@@ -0,0 +1,46 @@
+package s19_herenciaAndAbstract;
+
+import s19_herenciaAndAbstract.select.Opcion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectForm extends ElementoForm {
+
+    private List<Opcion> opciones;
+
+    public SelectForm(String nombre) {
+        super(nombre);
+        this.opciones = new ArrayList<Opcion>();
+    }
+
+    public SelectForm(String nombre, List<Opcion> opciones) {
+        super(nombre);
+        this.opciones = opciones;
+    }
+
+    public void addOpcion(Opcion opcion) {
+        this.opciones.add(opcion);
+    }
+
+    @Override
+    public String dibujarHTML() {
+        StringBuilder html = new StringBuilder("<select ");
+        html.append("name='")
+            .append(this.nombre)
+            .append("'>");
+        for (Opcion opcion : this.opciones) {
+            html.append("\n<option value='")
+                .append(opcion.getValor())
+                .append("'");
+            if(opcion.isSelected()) {
+                html.append(" selected");
+            }
+            html.append(">");
+            html.append(opcion.getNombre())
+                .append("</option>");
+        }
+        html.append("</select>");
+        return html.toString();
+    }
+}
---
+++ src/s19_herenciaAndAbstract/select/Opcion.java
@@ -0,0 +1,39 @@
+package s19_herenciaAndAbstract.select;
+
+public class Opcion {
+    private String valor;
+    private String nombre;
+    private boolean selected;
+
+    public Opcion() {}
+
+    public Opcion(String valor, String nombre, boolean selected) {
+        this.valor = valor;
+        this.nombre = nombre;
+        this.selected = selected;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+}
