6814a54e4fcbece2d2cdb66d000c1678b7eb7a52
Marcos-Del-Carmen-0010 <marcossachezdelcarmen@gmail.com>
2025-08-31 12:31:41 -0700

    ejercicio de estructura de datos

---
+++ src/s24_EstructuraDeDatos/ejercicio/Vuelo.java
@@ -0,0 +1,46 @@
+package s24_EstructuraDeDatos.ejercicio;
+
+import java.util.Date;
+
+public class Vuelo{
+    private String nombre;
+    private String origen;
+    private String destino;
+    private Date fechaLlegada;
+    private int numPasajeros;
+
+    public Vuelo() {
+    }
+
+    public Vuelo(String nombre, String origen, String destino, Date fechaLlegada, int numPasajeros) {
+        this.nombre = nombre;
+        this.origen = origen;
+        this.destino = destino;
+        this.fechaLlegada = fechaLlegada;
+        this.numPasajeros = numPasajeros;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public Date getFechaLlegada() {
+        return fechaLlegada;
+    }
+
+    public int getNumPasajeros() {
+        return numPasajeros;
+    }
+}
